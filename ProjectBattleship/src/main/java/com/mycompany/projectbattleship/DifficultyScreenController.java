9cd843bbe6d3b19abc3b937c1974965ab88a7a33
John Perez <john.perez.quesada@est.una.ac.cr>
2025-03-18 01:15:32 -0600

    Update (details on description)
    Easy mode board created but under development.
    Added name register to the game.
    
    Functionality updates:
    -Difficulty is not enabled unless a name is entered and saved.
    -Difficulty selection allows only one selection at a time.
    -Confirmation is enabled after selecting difficulty.
    -Medium and hard difficulty disabled in the code due to the non-existence of their corresponding scenarios.

--- ProjectBattleship/src/main/java/com/mycompany/projectbattleship/DifficultyScreenController.java
+++ ProjectBattleship/src/main/java/com/mycompany/projectbattleship/DifficultyScreenController.java
@@ -7,10 +7,12 @@ package com.mycompany.projectbattleship;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 
 /**
@@ -24,17 +26,41 @@ public class DifficultyScreenController implements Initializable {
     private RadioButton rbEasy, rbMedium, rbHard;
     
     @FXML
+    private TextField enterName;
+    String name;
+    
+    @FXML
     private Button confirmButton;
     
     private ToggleGroup rbGroup; 
     
     @FXML
-    private Button backDifficulty;
+    private Button backDifficulty, saveName;
     
     @FXML
     private void backToMain() throws IOException{
         App.setRoot("menuScreen");
     }
+    
+    @FXML
+    private void confirmSelection() throws IOException{
+        if (rbEasy.isSelected()){
+            App.setRoot("easyMode");
+        }
+    }
+    
+    @FXML
+    private void saveName(){
+        name = enterName.getText();
+        
+        if (!name.isEmpty()) {
+            saveName.setDisable(true);
+            enterName.setDisable(true);
+            rbEasy.setDisable(false);
+            rbMedium.setDisable(true); //AUN NO USAR
+            rbHard.setDisable(true); // AUN NO USAR
+        }
+    };
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -42,12 +68,21 @@ public class DifficultyScreenController implements Initializable {
         rbEasy.setToggleGroup(rbGroup);
         rbMedium.setToggleGroup(rbGroup);
         rbHard.setToggleGroup(rbGroup);
+
+        rbEasy.setDisable(true);
+        rbMedium.setDisable(true);
+        rbHard.setDisable(true);
+        confirmButton.setDisable(true);
+        saveName.setDisable(true);
         
+        enterName.textProperty().addListener((observable, oldValue, newValue) -> {
+            boolean isNotEmpty = !newValue.trim().isEmpty();
+            saveName.setDisable(!isNotEmpty);
+        });
+
+
         rbGroup.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
-            if (newValue != null) {
-                confirmButton.setDisable(false);
-            }
+            confirmButton.setDisable(newValue == null);
         });
     }    
-    
 }
